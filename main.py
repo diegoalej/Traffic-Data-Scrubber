190f72fca4b9aacbc79fb99e18a41fc6b7bdc4dc
Diego Hoyos <hoyosdiegoa@gmail.com>
2020-05-18 12:14:08 -0600

    Server config

--- main.py
+++ main.py
@@ -16,4 +16,4 @@ def getPlaces(placeid):
     return populartimes.get_id("AIzaSyCaKO2CFslQyLv58EgPScixNb4U2vxX_S0", placeid)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=False)
+    app.run(host='0.0.0.0', port=80)
