489a5934446c12cc71f2d58029450b1db0aeb906
Diego Hoyos <hoyosdiegoa@gmail.com>
2020-05-18 11:59:58 -0600

    Updated main method to host to 0.0.0.0

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import populartimes
 
 app = Flask(__name__)
 
@@ -10,5 +11,9 @@ def home():
 def about():
     return render_template("about.html")
 
+@app.route("/<placeid>", methods=["GET"])
+def getPlaces(placeid):
+    return populartimes.get_id("AIzaSyCaKO2CFslQyLv58EgPScixNb4U2vxX_S0", placeid)
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=False)
