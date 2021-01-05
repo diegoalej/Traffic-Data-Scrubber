22bb88444f0b028b04634b5df716a3b45213cfde
diegoalej <diegoh67@hotmail.com>
2021-01-05 10:55:59 -0700

    Update file to hide API Key

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def about():
 
 @app.route("/<placeid>", methods=["GET"])
 def getPlaces(placeid):
-    return populartimes.get_id("AIzaSyCaKO2CFslQyLv58EgPScixNb4U2vxX_S0", placeid)
+    return populartimes.get_id("API_KEY", placeid)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',  port=80)
